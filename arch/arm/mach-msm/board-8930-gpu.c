41e216e89aa52ef8e997f0db5b09fab156e53247
zoldyck07 <zoldyck07@ymail.com>
2014-07-20 00:36:35 +0700

    kgsl: change max KGSL powerlevels to 7

--- arch/arm/mach-msm/board-8930-gpu.c
+++ arch/arm/mach-msm/board-8930-gpu.c
@@ -116,6 +116,17 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
+                        .gpu_freq = 533000000,
+			.bus_freq = 6,
+			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 492000000,
+			.bus_freq = 5,
+			.io_fraction = 10,
+		},
+		{
+
                         .gpu_freq = 450000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
@@ -141,7 +152,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 5,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
